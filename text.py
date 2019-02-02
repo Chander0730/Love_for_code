e3d2988a5c4dfe2935214353e23d3c9fbcc6b3cb
Chander0730 <37927432+Chander0730@users.noreply.github.com>
2019-02-02 16:32:48 +0800

    Add files via upload

---
+++ text.py
@@ -0,0 +1,37 @@
+class User():
+    """docstring for User"""
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.login_attempts = 0
+
+    def describe_name(self):
+        print("fitst_name :", self.first_name, " last_name: ", self.last_name)
+
+    def greet_user(self):
+        print("hello, ", self.first_name, " ", self.last_name)
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+
+class Admin(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.privileges = Privileges()
+
+
+class Privileges():
+    def __init__(self, privileges=['can add post', 'can delete post', 'can ban user']):
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print(self.privileges)
+
+
+adminstrtor = Admin('fw', 'fge')
+adminstrtor.privileges.show_privileges()
